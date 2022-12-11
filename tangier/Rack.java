a10ff8c3f19472decac1b46267be8df428e6ee4d
Leonardo Lovera <leonard.lover@gmail.com>
2022-12-11 20:09:04 -0300

    Added pyramid setup

--- tangier/Rack.java
+++ tangier/Rack.java
@@ -9,11 +9,32 @@ class Rack extends JPanel {
 
     public Rack() {
         rack = new ArrayList<Ball>();
+        Ball b;
+
         for (int i = 0; i < 16; i++) {
-            Ball b = new Ball(i);
-            b.setPos(35 * i, 200);
+            b = new Ball(i);
             rack.add(b);
         }
+
+        int dx = 31;
+        int dy = 20;
+
+        rack.get(0).setPos(300, 400);
+        rack.get(9).setPos(600, 400);
+        rack.get(12).setPos(600 + dx, 400 - dy);
+        rack.get(1).setPos(600 + 2 * dx, 400 - 2 * dy);
+        rack.get(14).setPos(600 + 3 * dx, 400 - 3 * dy);
+        rack.get(5).setPos(600 + 4 * dx, 400 - 4 * dy);
+        rack.get(7).setPos(600 + dx, 400 + dy);
+        rack.get(15).setPos(600 + 2 * dx, 400 + 2 * dy);
+        rack.get(6).setPos(600 + 3 * dx, 400 + 3 * dy);
+        rack.get(11).setPos(600 + 4 * dx, 400 + 4 * dy);
+        rack.get(8).setPos(600 + 2 * dx, 400);
+        rack.get(3).setPos(600 + 3 * dx, 400 - dy);
+        rack.get(10).setPos(600 + 3 * dx, 400 + dy);
+        rack.get(4).setPos(600 + 4 * dx, 400 - 2 * dy);
+        rack.get(13).setPos(600 + 4 * dx, 400);
+        rack.get(2).setPos(600 + 4 * dx, 400 + 2 * dy);
     }
 
     public void paintComponent(Graphics g) {
