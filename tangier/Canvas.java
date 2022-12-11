ff6e56a153b23c3442f7336de03d5f63aad9d82b
clcser <clcser2021@udec.cl>
2022-12-10 21:38:54 -0300

    now balls are initialized in loop

--- tangier/Canvas.java
+++ tangier/Canvas.java
@@ -1,13 +1,17 @@
 package tangier;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.*;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
-import java.awt.*;
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 class Canvas extends JPanel implements MouseMotionListener, MouseListener, ActionListener {
     private Random rand;
@@ -15,9 +19,10 @@ class Canvas extends JPanel implements MouseMotionListener, MouseListener, Actio
     private CueBall cueball;
     private ArrayList<SolidBall> solidBall;
     private ArrayList<StripedBall> stripedBall;
+    private Color[] color;
     private Player player;
-    private SolidBall redSolid, orangeSolid, yellowSolid, greenSolid, blueSolid, purpleSolid, brownSolid;
-    private StripedBall redStriped, orangeStriped, yellowStriped, greenStriped, blueStriped, purpleStriped, brownStriped;
+    //private SolidBall redSolid, orangeSolid, yellowSolid, greenSolid, blueSolid, purpleSolid, brownSolid;
+    //private StripedBall redStriped, orangeStriped, yellowStriped, greenStriped, blueStriped, purpleStriped, brownStriped;
     private int mx;
     private int my;
 
@@ -29,27 +34,18 @@ class Canvas extends JPanel implements MouseMotionListener, MouseListener, Actio
         this.table = new Table();
         this.cueball = new CueBall();
         this.player = new Player();
-        
-        this.yellowSolid = new SolidBall(Color.yellow, "1");
-        this.blueSolid = new SolidBall(Color.blue, "2");
-        this.redSolid = new SolidBall(Color.red, "3");
-        this.purpleSolid = new SolidBall(new Color(0x9F2B68), "4");
-        this.orangeSolid = new SolidBall(Color.orange,"5");
-        this.greenSolid = new SolidBall(Color.green, "6");
-        this.brownSolid = new SolidBall(new Color(0x4B371C),"7");
-
-        this.yellowStriped = new StripedBall(Color.yellow, "9");
-        this.blueStriped = new StripedBall(Color.blue, "10");
-        this.redStriped = new StripedBall(Color.red, "11");
-        this.purpleStriped = new StripedBall(new Color(0x9F2B68), "12");
-        this.orangeStriped = new StripedBall(Color.orange, "13");
-        this.greenStriped = new StripedBall(Color.green, "14");
-        this.brownStriped = new StripedBall(new Color(0x4B371C), "15");
+        this.color = new Color[] {Color.yellow, Color.blue, Color.red, new Color(0x9F2B68), Color.orange, Color.green, new Color(0x4B371C)};
 
         this.solidBall = new ArrayList<SolidBall>();
-        this.solidBall.add(yellowSolid); this.solidBall.add(blueSolid); this.solidBall.add(redSolid); this.solidBall.add(purpleSolid); this.solidBall.add(orangeSolid); this.solidBall.add(greenSolid); this.solidBall.add(brownSolid);
         this.stripedBall = new ArrayList<StripedBall>();
-        this.stripedBall.add(yellowStriped); this.stripedBall.add(blueStriped); this.stripedBall.add(redStriped); this.stripedBall.add(purpleStriped); this.stripedBall.add(orangeStriped); this.stripedBall.add(greenStriped); this.stripedBall.add(brownStriped);
+
+        for(int i = 0; i < 7; i++) {
+            this.solidBall.add(new SolidBall(color[i], String.valueOf(i+1)));
+        }
+        for(int i = 0; i < 7; i++) {
+            this.stripedBall.add(new StripedBall(color[i], String.valueOf(i+9)));
+        }
+
         mx = 0;
         my = 0;
 
